fe940831df811204be72f2bae050ac49a518340d
thalessalla <101899988+thalessalla@users.noreply.github.com>
2024-01-23 17:01:28 -0300

    fix: LayoutDefault page

--- src/pages/layoutDefault/LayoutDefault.tsx
+++ src/pages/layoutDefault/LayoutDefault.tsx
@@ -1,37 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import DrawerAppBar from "../../components/header/Header";
-import NavBar from "../../components/navBar/NavBar";
-import Home from "../home/Home";
-import Login from "../login/Login";
-import Signup from "../signup/Signup";
-import AdminPage from "../adminPage/AdminPage";
-import Config from "../config/Config";
-import Confirmation from "../confirmation/Confirmation";
-import ThankyouPage from "../thankyouPage/ThankyouPage";
-import UserPage from "../userPage/UserPage";
-import Footer from "../../components/footer/Footer";
-
+import React from 'react';
 
 function LayoutDefault() {
   return (
     <>
-     {/* <BrowserRouter>
-      <DrawerAppBar />
-      <NavBar />      
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/adminpage" element={ <AdminPage />} />
-          <Route path="/config" element={ <Config />} />
-          <Route path="/userpage" element={ <UserPage />} />
-          <Route path="/confirmation" element={ <Confirmation />} />
-          <Route path="/thankyoupage" element={ <ThankyouPage />} />        
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-      < Footer /> */}
+
     </>
   );
 }
