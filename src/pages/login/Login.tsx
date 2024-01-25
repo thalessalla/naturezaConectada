1c59d20b3ca344ddd157c802f4ad8ef63d97ca68
thalessalla <101899988+thalessalla@users.noreply.github.com>
2024-01-25 00:22:55 -0300

    add: style forms and responsiveness

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -3,11 +3,45 @@ import "./Login.css"
 import { Link } from 'react-router-dom';
 import Input from "../../components/Form/Input";
 import SubmitButton from "../../components/Form/SubmitButton/SubmitButton";
-
+import axios from 'axios';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
 import { useState } from "react";
 
+interface FormData {
+  email: string;
+  password: string;
+}
 
-function Login() {
+
+const schema = yup.object().shape({
+  email: yup.string().email('Digite um endereço de e-mail válido').required('O e-mail é obrigatório'),
+  password: yup.string().required('Senha é obrigatória').oneOf(['string', 'number'], 'A senha deve ter caracteres e números'),
+});
+
+
+
+  const Login = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
+      resolver: yupResolver(schema),
+    });
+  
+    const onSubmit = async (data: FormData) => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3001/users?username=${data.email}&password=${data.password}`
+        );
+        if (response.data.length > 0) {
+          console.log('Login com sucesso:', response.data[0].id);
+        } else {
+          console.error('Credenciais inválidas');
+        }
+      } catch (error) {
+        console.error('Login falhou:', error);
+      }
+    };
+
 
 
 
@@ -24,26 +58,28 @@ function Login() {
 <div className="second-column-login">
 
 
-<form action="" method="post">
+<form action="<Link to='/UserPage'">
 <h2>Login</h2>
 
-<Input
-        type="email"
-        text="E-mail"
-        name="email"
-        placeholder="exemplo@email.com"
-        value=""
-      />
 
-      <Input
-        type="password"  
-        text="Senha"
-        name="senha"
-        value=""
-      />
-   
+<div>
 
-   <p>Ainda não tem cadastro? <Link className="link-signup" to="/signup">Cadastre-se! </Link></p>
+<label htmlFor="email">E-mail</label>  
+<input type="email" aria-label="Insira o seu email"
+            {...register('email')}/>
+</div>
+
+<div>
+
+<label htmlFor="password">Senha</label>  
+<input type="password" aria-label="Insira a senha" 
+            {...register('password')} />
+</div>
+
+<button type="submit" aria-label="Botão entrar" className="btn-submite">Entrar</button>
+
+
+   <p>Ainda não tem cadastro? <Link className="link-signup" to="/signup" >Cadastre-se! </Link></p>
 </form>
 
 
--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import "./Signup.css"
 import Input from "../../components/Form/Input"
 import { ChangeEvent } from "react";
+import React from 'react';
 import SubmitButton from "../../components/Form/SubmitButton/SubmitButton";
 import { SubmitHandler } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
@@ -10,10 +11,11 @@ import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 
 
+
 interface UserRegistrationResponse {
   id: number;
   error?: string;
-}
+}     
 
 interface UserFormData {
   username: string;
@@ -48,6 +50,9 @@ const schema = yup.object().shape({
 
 
 const Register = () => {
+
+ 
+
   const { register, handleSubmit, formState: { errors }, reset } = useForm<UserFormData>({
     resolver: yupResolver(schema),
   });
@@ -57,59 +62,103 @@ const Register = () => {
       const response: AxiosResponse<UserRegistrationResponse> = await axios.post('http://localhost:3001/users', data);
       console.log('User registered with ID:', response.data.id);
       alert('User cadastrado');
-     
+
       reset();
     } catch (error) {
       console.error('User não cadastrado');
     }
   };
 
+
+
   return (
-    <div className="container-signup">
-    <h1>Cadastre-se</h1>
-    <h2>Dados Pessoais</h2>
-   <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label>Nome Completo</label>
-          <input type="text" {...register('username', { required: true })} />
-          {errors.username && <span>Este campo é obrigatório</span>}
+    <div className="screen-signup">
+  <h1>Cadastre-se</h1>
+   
+   <form onSubmit={handleSubmit(onSubmit)} action="<Link to='/UserPage'">
+        <div className="container-signup">
+        <h2>Dados Pessoais</h2>
+          <div className="container-personal-data">
 
-          <label>E-mail</label>
-          <input type="email" {...register('email', { required: true })} />
+            <div>
+              <label>Nome Completo</label>
+          <input type="text" aria-label="Inisra seu nome completo" {...register('username',  { required: true })} />
           {errors.username && <span>Este campo é obrigatório</span>}
+            </div>
+          
 
-            <label>CPF</label>
-          <input type="text" {...register('cpf', { required: true })} />
+<div>
+  <label>E-mail</label>
+          <input type="email" aria-label="Insira o seu e-mail" {...register('email', { required: true })} />
           {errors.username && <span>Este campo é obrigatório</span>}
+</div>
+          
+
+<div>
+  <label>CPF</label>
+          <input type="text" placeholder="00000000000" aria-label="Insira seu CPF sem traços ou pontos" {...register('cpf', { required: true })} />
+          {errors.username && <span>Este campo é obrigatório</span>}
+</div>
+            
         
+        <div>
           <label>Senha</label>
-          <input type="password" {...register('password', { required: true })} />
+          <input type="password" aria-label="Insira a senha" {...register('password', { required: true })} />
           {errors.password && <span>Este campo é obrigatório</span>}
-
-          <h3>Endereço</h3>
-
-          <label>Cep</label>
-          <input type="text" placeholder="00000-000" {...register('cep', { required: true })} />
-          {errors.password && <span>Este campo é obrigatório</span>}
-
-          <label>Cidade</label>
-          <input type="text" {...register('cidade', { required: true })} />
-          {errors.password && <span>Este campo é obrigatório</span>}
-
-          <label>Rua</label>
-          <input type="string" {...register('rua', { required: true })} />
-          {errors.password && <span>Este campo é obrigatório</span>}
-
-          <label>Numero</label>
-          <input type="number" {...register('numero', { required: true })} />
-          {errors.password && <span>Este campo é obrigatório</span>}
-
-          <label>Complemento</label>
-          <input type="text" {...register('complemento', { required: true })} />
-          {errors.password && <span>Este campo é obrigatório</span>}
-
         </div>
-        <button type="submit">Registrar</button>
+          
+          </div>
+
+
+<hr />
+
+   <h2>Endereço</h2>
+
+
+<div>
+         
+
+
+<div>
+<label>Cep</label>
+          <input type="text" placeholder="00000-000" aria-label="Insira o Cep de onde você mora" {...register('cep', { required: true })} />
+          {errors.password && <span>Este campo é obrigatório</span>}
+</div>
+       
+       <div>
+       <label>Cidade</label>
+          <input type="text" aria-label="Insira a cidade a onde você reside" {...register('cidade', { required: true })} />
+          {errors.password && <span>Este campo é obrigatório</span>}
+       </div>
+
+          
+<div>
+<label>Rua</label>
+          <input type="string" aria-label="Rua a onde você mora" {...register('rua', { required: true })} />
+          {errors.password && <span>Este campo é obrigatório</span>}
+
+</div>
+          
+
+<div>
+ <label>Numero</label>
+          <input type="number" aria-label="insira o numero de sua residencia" {...register('numero', { required: true })} />
+          {errors.password && <span>Este campo é obrigatório</span>}
+
+</div>
+         
+
+<div>
+ <label>Complemento</label>
+          <input type="text" aria-label="Insira um complemento caso necessário" {...register('complemento', { required: true })} />
+          {errors.password && <span>Este campo é obrigatório</span>}
+
+</div>
+         
+          </div>
+          <button type="submit" aria-label="Botão Registrar" className="btn-submite">Registrar</button>
+        </div>
+        
       </form>
     </div>
   );
