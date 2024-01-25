3c2347d1733c94e613d66825920eeebbdbf39aec
thalessalla <101899988+thalessalla@users.noreply.github.com>
2024-01-25 08:30:49 -0300

    fix: validation signup form

--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -29,6 +29,21 @@ interface UserFormData {
   complemento?: string;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const schema = yup.object().shape({
   username: yup.string().required('Nome do usuário é obrigatório'),
   password: yup
@@ -40,7 +55,7 @@ const schema = yup.object().shape({
     .matches(/^\d{11}$/, 'Digite seu CPF corretamente') 
     .required('Campo obrigatório'),
   cep: yup.string()
-    .matches(/^\d{5}-\d{3}$/, 'CEP inválido') 
+    .matches(/^\d{8}$/, 'CEP inválido') 
     .required('Campo obrigatório'),
   cidade: yup.string().required('Nome da Cidade é obrigatório'),
   rua: yup.string().required('Campo de rua é obrigatório'),
@@ -59,6 +74,18 @@ const Register = () => {
   
   const onSubmit: SubmitHandler<UserFormData> = async (data) => {
     try {
+      const cpfCheck = await axios.get(`http://localhost:3001/users?cpf=${data.cpf}`);
+      const emailCheck = await axios.get(`http://localhost:3001/users?email=${data.email}`);
+
+      if (cpfCheck.data.length > 0) {
+        alert('CPF já cadastrado');
+        return;
+      }
+
+      if (emailCheck.data.length > 0) {
+        alert('E-mail já cadastrado');
+        return;
+      }
       const response: AxiosResponse<UserRegistrationResponse> = await axios.post('http://localhost:3001/users', data);
       console.log('User registered with ID:', response.data.id);
       alert('User cadastrado\n\nBem-vindo à área do usuário');
@@ -77,7 +104,7 @@ const Register = () => {
     <div className="screen-signup">
   <h1>Cadastre-se</h1>
    
-   <form onSubmit={handleSubmit(onSubmit)} action="<Link to='/UserPage'">
+   <form onSubmit={handleSubmit(onSubmit)}>
         <div className="container-signup">
         <h2>Dados Pessoais</h2>
           <div className="container-personal-data">
@@ -85,28 +112,28 @@ const Register = () => {
             <div>
               <label>Nome Completo</label>
           <input type="text" aria-label="Inisra seu nome completo" {...register('username',  { required: true })} />
-          {errors.username && <span>Este campo é obrigatório</span>}
+          {errors.username && <span>Insira o nome completo</span>}
             </div>
           
 
 <div>
   <label>E-mail</label>
-          <input type="email" aria-label="Insira o seu e-mail" {...register('email', { required: true })} />
-          {errors.username && <span>Este campo é obrigatório</span>}
+          <input type="email" aria-label="Insira o seu e-mail" placeholder="exemplo@exemplo.com" {...register('email', { required: true })} />
+          {errors.email && <span>Campo obrigatório</span>}
 </div>
           
 
 <div>
   <label>CPF</label>
           <input type="text" placeholder="00000000000" aria-label="Insira seu CPF sem traços ou pontos" {...register('cpf', { required: true })} />
-          {errors.username && <span>Este campo é obrigatório</span>}
+          {errors.cpf && <span>Campo obrigatório</span>}
 </div>
             
         
         <div>
           <label>Senha</label>
           <input type="password" aria-label="Insira a senha" {...register('password', { required: true })} />
-          {errors.password && <span>Este campo é obrigatório</span>}
+          {errors.password && <span>A senha deve ter caracteres e números</span>}
         </div>
           
           </div>
@@ -123,21 +150,21 @@ const Register = () => {
 
 <div>
 <label>Cep</label>
-          <input type="text" placeholder="00000-000" aria-label="Insira o Cep de onde você mora" {...register('cep', { required: true })} />
-          {errors.password && <span>Este campo é obrigatório</span>}
+          <input type="text" placeholder="00000000" aria-label="Insira o Cep de onde você mora" {...register('cep', { required: true })} />
+          {errors.cep && <span>Campo obrigatório, digite de forma correta</span>}
 </div>
        
        <div>
        <label>Cidade</label>
           <input type="text" aria-label="Insira a cidade a onde você reside" {...register('cidade', { required: true })} />
-          {errors.password && <span>Este campo é obrigatório</span>}
+          {errors.cidade && <span>Campo obrigatório</span>}
        </div>
 
           
 <div>
 <label>Rua</label>
           <input type="string" aria-label="Rua a onde você mora" {...register('rua', { required: true })} />
-          {errors.password && <span>Este campo é obrigatório</span>}
+          {errors.rua && <span>Campo obrigatório</span>}
 
 </div>
           
@@ -145,7 +172,7 @@ const Register = () => {
 <div>
  <label>Numero</label>
           <input type="number" aria-label="insira o numero de sua residencia" {...register('numero', { required: true })} />
-          {errors.password && <span>Este campo é obrigatório</span>}
+          {errors.numero && <span>Campo obrigatório</span>}
 
 </div>
          
@@ -153,7 +180,7 @@ const Register = () => {
 <div>
  <label>Complemento</label>
           <input type="text" aria-label="Insira um complemento caso necessário" {...register('complemento', { required: true })} />
-          {errors.password && <span>Este campo é obrigatório</span>}
+         
 
 </div>
          
